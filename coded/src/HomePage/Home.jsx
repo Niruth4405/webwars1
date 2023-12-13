c4f22779a219c8f55d24f9cf140c613614343172
Niruth Ananth <69809343+Niruth4405@users.noreply.github.com>
2023-12-13 17:26:05 +0530

    Home.jsx updated

--- coded/src/HomePage/Home.jsx
+++ coded/src/HomePage/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
-
+import "../components/About"
+import "../components/Carousal"
+import "../components/Faq"
 
 function Home() {
   return (
     <>
-    
+    <Carousal/>
+    <Faq/>
+    <About/>
     </>
   );
 }
