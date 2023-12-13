dd3d63329d6af97b0038698058a03e4861b4acb2
Niruth Ananth <69809343+Niruth4405@users.noreply.github.com>
2023-12-13 17:33:04 +0530

    main updated

--- coded/src/main.jsx
+++ coded/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import "../src/components/Navbar.jsx"
+import "../src/components/Footer.jsx"
+import Navbar from '../src/components/Navbar.jsx'
+import Footer from '../src/components/Footer.jsx'
  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Navbar/>
     <App />
+    <Footer/>
   </React.StrictMode>,
 )
